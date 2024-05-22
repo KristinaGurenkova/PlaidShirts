6fa462bb95a159fd52efdfb4e919a09f9acbc8f6
KristinaGurenkova <116495111+KristinaGurenkova@users.noreply.github.com>
2024-05-22 15:53:56 +0300

    v2

--- Shop/Class1.cs
+++ Shop/Class1.cs
@@ -7,19 +7,31 @@ using Shop.DataBase;
 
 namespace Shop
 {
+	/// <summary>
+	/// Класс Shop предназначен для работы с базой данных
+	/// </summary>
 	public class StoreOperations
 	{
 		PlaidShirtsEntities shirtsEntities = new PlaidShirtsEntities();
+		/// <summary>
+		/// Метод InputHall предназначен для вывода списка товаров в зале
+		/// </summary>
 		public List<DataBase.Hall> InputHall()
 		{
 			var listProd = shirtsEntities.Hall.ToList();
 			return listProd;
 		}
+		/// <summary>
+		/// Метод InputStorage предназначен для вывода списка товаров на складе
+		/// </summary>
 		public List<DataBase.Storage> InputStorage()
 		{
 			var listProd = shirtsEntities.Storage.ToList();
 			return listProd;
 		}
+		/// <summary>
+		/// Метод Ticket предназначен для формирования чека
+		/// </summary>
 		public List<DataBase.Basket> Ticket()
 		{
 			var listProd = shirtsEntities.Basket.ToList();
